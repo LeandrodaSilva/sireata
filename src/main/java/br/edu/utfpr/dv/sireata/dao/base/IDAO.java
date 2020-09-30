d855b5974863e2c42ee70f6bf3354a898c4008e6
LEANDRO DA SILVA <ld_silva13@hotmail.com>
2020-09-29 21:12:24 -0300

    Criado interface para os DAO

--- src/main/java/br/edu/utfpr/dv/sireata/dao/base/IDAO.java
+++ src/main/java/br/edu/utfpr/dv/sireata/dao/base/IDAO.java
@@ -1,9 +1,7 @@
 package br.edu.utfpr.dv.sireata.dao.base;
 
-import br.edu.utfpr.dv.sireata.model.Ata;
-
 import java.sql.SQLException;
 
-public interface IDAO<D> {
-    public abstract D buscarPorId(int id) throws SQLException;
+public interface IDAO<MODEL> {
+    public abstract MODEL buscarPorId(int id) throws SQLException;
 }
